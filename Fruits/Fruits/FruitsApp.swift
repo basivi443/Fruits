18b726f3898ee874bd1746a9fcd4cf05b405a65b
basivireddy <basivi.info016@gmail.com>
2024-06-15 08:21:24 +0530

    initital commit

---
+++ Fruits/Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Basivi Reddy on 15/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
